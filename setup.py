b370056421ab1825aec64442cc25cce303c14fb0
NaveenVinayak S <27666222+NaveenVinayakS@users.noreply.github.com>
2021-09-15 21:43:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 '''setup.py is a python file, the presence of which is an indication that the module/package you are about to install has likely been packaged and distributed with Distutils, which is the standard for distributing Python Modules.
 This allows you to easily install Python packages. Often it's enough to write:
 $ pip install . 
-pip will use setup.py to install your module. Avoid calling setup.py directly.
+pip will use setup.py to install your module. Avoid calling setup.py directly
 https://stackoverflow.com/questions/1471994/what-is-setup-py
 '''
 
